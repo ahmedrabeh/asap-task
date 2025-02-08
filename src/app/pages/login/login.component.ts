8d7c28953c171be48b782bf927008720b3e6d1ff
Ahmed Rabeh <ahmed.rabeh@iqranetwork.com>
2025-02-08 17:39:42 +0200

    Push ASAP task to github

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  showPassword = false;
+
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (user) => {
+        console.log('Login successful:', user);
+        this.router.navigate(['/home']); // Redirect after login
+      },
+      error: (err) => {
+        this.errorMessage = err.message;
+      }
+    });
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+}
